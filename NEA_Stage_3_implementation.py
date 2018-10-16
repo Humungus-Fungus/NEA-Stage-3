4bc69f9383ffcfbdc9d9b9608ae27b7e9e3e01b5
Humungus Fungus <devarshdinesh@gmail.com>
2018-10-16 14:03:58 +0400

    Bit of progress

--- NEA_Stage_3_implementation.py
+++ NEA_Stage_3_implementation.py
@@ -1,18 +1,18 @@
 # NEA Stage 3: Implementation
 class NEA(object):
-    def implementation():
+    def implementation(self):
         # Predefined process: Input
-        def input():
+        def input_data():
             # All the different types of coin, bags, coin weights. All these values are linked.
-            coin_types = [2,  1,  0.5, 0.2, 0.1,  0.05, 0.02, 0.01] # In pounds
-            bag_values = [20, 20,  10, 10,  5,    5,    1,    1] # In pounds
-            coin_weights=[12, 8.75, 8, 5,   6.5,  3.25, 7.12, 3.56] # In grams
+            coin_types = [2,    1,    0.5, 0.2, 0.1,  0.05, 0.02, 0.01]  # In pounds
+            bag_values = [20,   20,   10,  10,  5,    5,    1,    1]  # In pounds
+            coin_weights = [12, 8.75, 8,   5,   6.5,  3.25, 7.12, 3.56]  # In grams
 
             validity = False
 
             # While validity is not true...
             while not validity:
-                v_name, coin_type, bag_weight = input("Name: "), input("Coin Type: "), input("Bag Weigth: ")
+                v_name, coin_type, bag_weight = input("Name: "), input("Coin Type: "), input("Bag Weight: ")
 
                 if coin_type not in coin_types:
                     # Not changing validity
@@ -20,16 +20,16 @@ class NEA(object):
                 elif coin_type in coin_types:
                     validity = True
 
-            # A way to connect all the three values. Mega because it's important. Pos because it's the position in the list.
-            mega_pos = coin_types.index()
-
-            bag_value = bag_values[mega_pos]
-            coin_weight = coin_weights[mega_pos]
-        
         # coin type has been validated!
 
-        #Predefined process: Calculations
-        def calculations():
-            # Correct Bag Weight (cbw)
-            cbw = (bag_value/coin_type) * coin_weight
-        
+        # Predefined process: Calculations
+            def calculations():
+                # Connecting all the three values. Mega because it's important. Pos because it's the position in list.
+                mega_pos = coin_types.index(coin_type)
+
+                bag_value = bag_values[mega_pos]
+                coin_weight = coin_weights[mega_pos]
+                # Correct Bag Weight (cbw)
+                cbw = (bag_value/coin_type) * coin_weight
+
+        # calculation function has been done!
